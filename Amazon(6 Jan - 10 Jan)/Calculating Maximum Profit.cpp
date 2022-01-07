405a633ed0bad19e15ab44bf85e95089bdf878a0
harshul_21 <56495746+harshul21@users.noreply.github.com>
2022-01-07 15:31:41 +0530

    Update Calculating Maximum Profit.cpp

--- Amazon(6 Jan - 10 Jan)/Calculating Maximum Profit.cpp
+++ Amazon(6 Jan - 10 Jan)/Calculating Maximum Profit.cpp
@@ -1,1 +1,31 @@
+//Problem Link-https://practice.geeksforgeeks.org/problems/maximum-profit4657/1
+class Solution {
+  public:
+    int maxProfit(int K, int N, int A[]) {
+        int dp[K+1][N];
+        for(int i=0;i<N;i++){
+        	dp[0][i]=0;
+        }
+        for(int i=0;i<=K;i++){
+        	dp[i][0]=0;
+        }
+        for(int t=1;t<=K;t++){
+        	for(int d=1;d<N;d++){
+        		int mx=dp[t][d-1];
 
+        		for(int pd=0;pd<d;pd++){
+        			int ptilltm1=dp[t-1][pd];
+        			int ptth=A[d]-A[pd];
+
+        			if(ptilltm1+ptth>mx){
+        				mx=ptth+ptilltm1;
+        			}
+        		}
+        		dp[t][d]=mx;
+        	}
+        }
+        return dp[K][N-1];
+    }
+};
+
+//More Optimised Approach
