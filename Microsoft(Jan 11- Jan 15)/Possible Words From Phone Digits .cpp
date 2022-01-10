58044f77dd8054ff03f87e11cf27c45c5482b270
harshul_21 <56495746+harshul21@users.noreply.github.com>
2022-01-10 23:29:00 +0530

    Update Possible Words From Phone Digits .cpp

--- Microsoft(Jan 11- Jan 15)/Possible Words From Phone Digits .cpp
+++ Microsoft(Jan 11- Jan 15)/Possible Words From Phone Digits .cpp
@@ -1,1 +1,26 @@
-
+Problem Link:-https://practice.geeksforgeeks.org/problems/possible-words-from-phone-digits-1587115620/1/
+class Solution
+{
+    public:
+    //Function to find list of all words possible by pressing given numbers.
+    void dfs(int idx,int a[],int N,vector<string> phone,vector<string> &ans,string &ds){
+        if(ds.size()==N){
+            ans.push_back(ds);
+            return;
+        }
+        string ch=phone[a[idx]];
+        for(int i=0;i<ch.length();i++){
+            ds.push_back(ch[i]);
+            dfs(idx+1,a,N,phone,ans,ds);
+            ds.pop_back();
+        }
+    }
+    vector<string> possibleWords(int a[], int N)
+    {
+        vector<string> phone={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> ans;
+        string ds="" ;
+        dfs(0,a,N,phone,ans,ds);
+        return ans;
+    }
+};
