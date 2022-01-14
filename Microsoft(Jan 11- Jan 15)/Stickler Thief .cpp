73867f9722a54d87f766d8b9aaee26c85df58524
harshul_21 <56495746+harshul21@users.noreply.github.com>
2022-01-14 23:21:58 +0530

    Create Stickler Thief .cpp

---
+++ Microsoft(Jan 11- Jan 15)/Stickler Thief .cpp
@@ -0,0 +1,18 @@
+// Same as house robber in leetcode
+//Problem Link:-https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1/
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int dp[n];
+        dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+        for(int i=2;i<=n;i++){
+            dp[i]=max(dp[i-1],arr[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+};  
