2da5ee752f3bbb338ba18f0f0efec8f094d0fbb1
harshul_21 <56495746+harshul21@users.noreply.github.com>
2022-01-15 11:21:07 +0530

    Create Generate Binary Numbers .cpp

---
+++ Microsoft(Jan 11- Jan 15)/Generate Binary Numbers .cpp
@@ -0,0 +1,28 @@
+//Problem Link:- https://practice.geeksforgeeks.org/problems/generate-binary-numbers-1587115620/1/#
+//Function to generate binary numbers from 1 to N using a queue.
+string dectobinary(int n){
+    string s;
+    for(int i=31;i>=0;i--){
+        int k=n>>i;
+        if(k&1){
+            s.push_back('1');
+        }
+        else{
+            s.push_back('0');
+        }
+    }
+    for(int i=0;i<32;i++){
+        if(s[i]=='1'){
+            return s.substr(i);
+        }
+    }
+}
+vector<string> generate(int N)
+{
+	vector<string> ans;
+	for(int i=1;i<=N;i++){
+	    string temp=dectobinary(i);
+	    ans.push_back(temp);
+	}
+	return ans;
+}
