658be4eed81de7481c5b49bf4950b0b3ea8bfc18
harshul_21 <56495746+harshul21@users.noreply.github.com>
2022-01-18 15:31:37 +0530

    Create Longest Arithmetic Progression .cpp

---
+++ Adobe (Jan 16- Jan 20)/Longest Arithmetic Progression .cpp
@@ -0,0 +1,30 @@
+//Problem Link: - https://practice.geeksforgeeks.org/problems/longest-arithmetic-progression1019/1/#
+class Solution{   
+public:
+    int lengthOfLongestAP(int arr[], int n) {
+        // code here
+        if(n==1){
+            return n;
+        }
+        if(n==2){
+            return n;
+        }
+        
+        vector<unordered_map<int,int>> dp(n);
+        int ans=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                int diff=arr[i]-arr[j];
+                
+                if(dp[j].find(diff)==dp[j].end()){
+                    dp[i][diff]=2;
+                }
+                else{
+                    dp[i][diff]=dp[j][diff]+1;
+                }
+                ans=max(ans,dp[i][diff]);
+            }
+        }
+        return ans;
+    }
+};
